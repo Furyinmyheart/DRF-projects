4ccdd2d95d063efb7612aa1f1e9889a33587f0dc
Mihail Bokohod <Bokohod@bk.ru>
2017-07-29 00:02:47 +0300

    Add files via upload

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Channel, Campaign
 
-# Register your models here.
+
+class ChannelModelAdmin(admin.ModelAdmin):
+    list_display = [Campaign, 'Name', 'Slug', 'BidTypes']
+    class Meta:
+        model = Channel
+
+
+class CampaignModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'Name', 'Bid']
+    class Meta:
+        model = Campaign
+
+admin.site.register(Channel, ChannelModelAdmin)
+
+admin.site.register(Campaign, CampaignModelAdmin)
--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,6 +1,6 @@
+from rest_framework import serializers
 from .models import Channel
 from .models import Campaign
-from rest_framework import serializers
 
 
 class ChannelSerializer(serializers.HyperlinkedModelSerializer):
